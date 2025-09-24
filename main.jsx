ff4605d22224d006a0af57cfbae67148c8976c1e
abbywilson11 <awils141@uottawa.ca>
2025-09-24 10:40:14 -0400

    Update main.jsx
    added what i wrote

--- main.jsx
+++ main.jsx
@@ -1,3 +1,4 @@
+// i wrote the imports 
 import React from "react";
 import "leaflet/dist/leaflet.css"; //import leaflet for map of data
 import ReactDOM from "react-dom/client"; //connects react to the actual application
